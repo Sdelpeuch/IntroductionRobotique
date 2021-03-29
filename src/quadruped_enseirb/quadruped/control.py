ac28659d3800465515e5cede738bcf2d6a64a73f
Antoine Pringalle <pringalle.a@gmail.com>
2021-03-29 17:36:29 +0200

    walking with interpolation

--- src/quadruped_enseirb/quadruped/control.py
+++ src/quadruped_enseirb/quadruped/control.py
@@ -153,19 +153,23 @@ def draw(t):
 def step(t, patte_num, speed_x, speed_y):
     d = 0.120
     z0 = -0.05
-    initiale = ((-d, d, z0), (-d, -d, z0), (d, -d, z0), (d, d, z0))
+
+    initiale = [(-d, d, z0), (-d, -d, z0), (d, -d, z0), (d, d, z0)]
     high = (initiale[patte_num][0] + speed_x / 2, initiale[patte_num][1] + speed_y / 2, initiale[patte_num][2] + 0.05)
-    end = (initiale[patte_num][0] - speed_x, initiale[patte_num][1] - speed_y, initiale[patte_num][2] + 0)
+    end = (initiale[patte_num][0] + speed_x / 2, initiale[patte_num][1] + speed_y / 2, initiale[patte_num][2])
 
     interpolator = interpolation.LinearSpline3D()
-    #print(initiale[patte_num])
-    interpolator.add_entry(1, * initiale[patte_num])
-    interpolator.add_entry(2, * high)
-    interpolator.add_entry(3, * end)
-    interpolator.add_entry(4, * end)
+
+    interpolator.add_entry(0, * initiale[patte_num])
+    interpolator.add_entry(1, * high)
+    interpolator.add_entry(2, * end)
+    interpolator.add_entry(3, * initiale[patte_num])
     x, y, z = interpolator.interpolate(t % 3)
 
-    return (x, y, z)
+    print(t)
+
+    return x, y, z
+
 
 def legs(leg1, leg2, leg3, leg4):
     """
@@ -214,68 +218,29 @@ def legs(leg1, leg2, leg3, leg4):
 
 leg1 = []
 
-
 def walk_2(t, speed_x, speed_y, speed_rotation):
-    d = 0.120
-    z0 = -0.05
-    """
-    python simulator.py -m walk
-
-    Le but est d'intégrer tout ce que nous avons vu ici pour faire marcher le robot
-
-    - Sliders: speed_x, speed_y, speed_rotation, la vitesse cible du robot
-    - Entrée: t, le temps (secondes écoulées depuis le début)
-            speed_x, speed_y, et speed_rotation, vitesses cibles contrôlées par les sliders
-    - Sortie: un tableau contenant les 12 positions angulaires cibles (radian) pour les moteurs
-    """
-    targets = [0] * 12
-    # Position initiale
-    initiale = [(-d, d, z0), (-d, -d, z0), (d, -d, z0), (d, d, z0)]
-
-    if t == 0:
-        return legs(initiale[0], initiale[1], initiale[2], initiale[3])
 
     # Déterminer la position des pattes en fonction de t
-    avancer_x  = speed_x
-    avancer_y = speed_y
-    print("avancer_x = {0}\n avancer_y = {1}".format(avancer_x, avancer_y))
-    avancer_z = 0
-    if speed_x != 0 or speed_y != 0:
-        avancer_z = 0.03
-    step_num = int((t * 15) % 4)
-    # print("t : {0}".format(t))
-
-    if step_num == 0:
-        patte1 = step(t, 0, speed_x, speed_y)
-        patte3 = step(t, 2, speed_x, speed_y)
-        patte2 = initiale[2]
-        patte4 = initiale[3]
-
-    elif step_num == 1:
-        #print("step {0}".format(step))
-        patte1 =
-        patte2 = initiale[1]
-        patte3 = (initiale[2][0] + avancer_x / 2, initiale[2][1] + avancer_y / 2, initiale[2][2] + avancer_z)
-        patte4 = initiale[3]
-
-
-    elif step_num == 2:
-        # print("step {0}".format(step))
-        patte1 = (initiale[0][0] + avancer_x / 2, initiale[0][1] + avancer_y / 2, initiale[0][2])
-        patte2 = (initiale[1][0] - avancer_x, initiale[1][1] - avancer_y, initiale[1][2])
-        patte3 = (initiale[2][0] + avancer_x / 2, initiale[2][1] + avancer_y / 2, initiale[2][2])
-        patte4 = (initiale[3][0] - avancer_x, initiale[3][1] - avancer_y, initiale[3][2])
-
-
-    else:  # step == 3
-        # print("step {0}".format(step))
-        patte1 = initiale[0]
-        patte2 = (initiale[1][0] + avancer_x / 2, initiale[1][1] + avancer_y / 2, initiale[1][2] + avancer_z)
-        patte3 = initiale[2]
-        patte4 = (initiale[3][0] + avancer_x / 2, initiale[3][1] + avancer_y / 2, initiale[3][2] + avancer_z)
+    # print("avancer_x = {0}\navancer_y = {1}".format(speed_x, speed_y))
+    patte1 = step(t+1, 0, speed_x, speed_y)
+    patte2 = step(t, 1, speed_x, speed_y)
+    patte3 = step(t+1, 2, speed_x, speed_y)
+    patte4 = step(t, 3, speed_x, speed_y)
 
     targets = legs(patte1, patte2, patte3, patte4)
-    # print('\np1:', patte1, '\np2:', patte2, '\np3:', patte3, '\np4:', patte4)
+
+
+    if False:
+        x1, y1, z1 = patte3
+        leg1.append([x1, y1, z1])
+        tmp = np.array(leg1)
+        plt.clf()
+        plt.plot(tmp.T[0], color = 'red')
+        plt.plot(tmp.T[1], color='green')
+        plt.plot(tmp.T[2], color='blue')
+        plt.grid()
+        plt.pause(0.01)
+
     return targets
 
 
@@ -340,7 +305,7 @@ def walk(t, speed_x, speed_y, speed_rotation):
         patte3 = initiale[2]
         patte4 = (initiale[3][0] + avancer_x / 2, initiale[3][1] + avancer_y / 2, initiale[3][2] + avancer_z)
 
-    if True:
+    if False:
         x1, y1, z1 = patte1
         leg1.append([x1, y1, z1])
         tmp = np.array(leg1)
