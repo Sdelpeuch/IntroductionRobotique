91b58df052e6bc2c42a07aebf7022e7f0d933d00
Nathan Decou <nathan.decou@gmail.com>
2021-03-29 15:39:09 +0200

    control : debut walk

--- src/quadruped_enseirb/quadruped/control.py
+++ src/quadruped_enseirb/quadruped/control.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+
 if __package__ is None or __package__ == '':
     import interpolation
 else:
@@ -8,6 +9,7 @@ else:
 l0 = 0.045
 l1 = 0.065
 l2 = 0.087
+
 
 def sandbox(t):
     """
@@ -23,10 +25,11 @@ def sandbox(t):
     """
 
     # Par exemple, on envoie un mouvement sinusoidal
-    targets = [0]*12
+    targets = [0] * 12
     targets[0] = np.sin(t)
 
     return targets
+
 
 def direct(a, b, c):
     """
@@ -42,34 +45,37 @@ def direct(a, b, c):
     - Sortie: x, y, z, la position du bout de la patte
     """
 
-    T0 = a # Theta 0
-    T1 = np.pi + b # Theta 1
-    T2 = c + np.pi # Theta 2
+    T0 = a  # Theta 0
+    T1 = np.pi + b  # Theta 1
+    T2 = c + np.pi  # Theta 2
 
     # point O
     O = np.array([[0], [0], [0]])
-    #print("O:", O)
+    # print("O:", O)
 
     # point A
     A = np.array([[l0 * math.cos(T0)], [l0 * math.sin(T0)], [0]]) + O
-    #print("A:", A)
+
+    # print("A:", A)
 
     # point B
     def rotationZ(t):
-        return np.array([[np.cos(t), -np.sin(t), 0], [np.sin(t), np.cos(t), 0],[0, 0, 1]])
+        return np.array([[np.cos(t), -np.sin(t), 0], [np.sin(t), np.cos(t), 0], [0, 0, 1]])
 
     def rotationX(t):
-        return np.array([[1, 0, 0], [0, np.cos(t), -np.sin(t)],[0, np.sin(t), np.cos(t)]])
+        return np.array([[1, 0, 0], [0, np.cos(t), -np.sin(t)], [0, np.sin(t), np.cos(t)]])
 
     B = np.dot(rotationZ(T0), np.array([[l1 * np.cos(T1 - np.pi)], [0], [l1 * np.sin(T1 - np.pi)]])) + A
-    #print("B:", B)
+
+    # print("B:", B)
 
     # point C
     def rotationY(t):
-        return np.array([[np.cos(t), 0, np.sin(t)], [0, 1, 0],[-np.sin(t), 0, np.cos(t)]])
+        return np.array([[np.cos(t), 0, np.sin(t)], [0, 1, 0], [-np.sin(t), 0, np.cos(t)]])
 
     # point M
-    M = np.dot(rotationZ(T0), np.dot(rotationY(np.pi-T1), np.array([[l2 * np.cos(np.pi - T2)], [0], [l2 * np.sin(np.pi - T2)]]))) + B
+    M = np.dot(rotationZ(T0),
+               np.dot(rotationY(np.pi - T1), np.array([[l2 * np.cos(np.pi - T2)], [0], [l2 * np.sin(np.pi - T2)]]))) + B
     # print("M:", M)
 
     # points = [O, A, B, M]
@@ -82,13 +88,15 @@ def direct(a, b, c):
 
     return [M[0], M[1], M[2]]
 
+
 def alKashi(a, b, c):
-    angle = (a**2 + b**2 - c**2) / (2 * a * b)
+    angle = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
     if angle > 1:
         angle = 1
     elif angle < -1:
         angle = -1
     return math.acos(angle)
+
 
 def inverse(x, y, z):
     """
@@ -107,23 +115,24 @@ def inverse(x, y, z):
     side = 1
 
     theta0 = math.atan2(y, x)
-    AH = math.sqrt(x**2 + y**2) - l0
-    AM = math.sqrt(AH**2 + z**2)
+    AH = math.sqrt(x ** 2 + y ** 2) - l0
+    AM = math.sqrt(AH ** 2 + z ** 2)
     theta2 = side * (np.pi - alKashi(l1, l2, AM))
 
     if (AM == 0):
-        theta1 = 0 # Peu importe
+        theta1 = 0  # Peu importe
     else:
         theta1 = side * alKashi(AM, l1, l2) + math.atan2(z, AH)
     return [theta0, theta1, theta2]
 
+
 def draw(t):
     interpolator = interpolation.LinearSpline3D()
-    interpolator.add_entry(0,0.05,0,0)
-    interpolator.add_entry(1,0.05,0,0.1)
+    interpolator.add_entry(0, 0.05, 0, 0)
+    interpolator.add_entry(1, 0.05, 0, 0.1)
     interpolator.add_entry(2, 0.05, 0.1, 0.1)
     interpolator.add_entry(3, 0.05, 0, 0)
-    x, y, z = interpolator.interpolate(t%3)
+    x, y, z = interpolator.interpolate(t % 3)
 
     """
     python simulator.py -m draw
@@ -139,6 +148,7 @@ def draw(t):
 
     return inverse(x, y, z)
 
+
 def legs(leg1, leg2, leg3, leg4):
     """
     python simulator.py -m legs
@@ -149,11 +159,11 @@ def legs(leg1, leg2, leg3, leg4):
     - Entrée: des positions cibles (tuples (x, y, z)) pour le bout des 4 pattes
     - Sortie: un tableau contenant les 12 positions angulaires cibles (radian) pour les moteurs
     """
-    targets = [0]*12
-    offset = math.sqrt(0.040*0.040/2)
+    targets = [0] * 12
+    offset = math.sqrt(0.040 * 0.040 / 2)
 
     def rotation(t, x, y):
-        return np.dot(np.array([[np.cos(t), -np.sin(t)], [np.sin(t), np.cos(t)]]), np.array([[x],[y]]))
+        return np.dot(np.array([[np.cos(t), -np.sin(t)], [np.sin(t), np.cos(t)]]), np.array([[x], [y]]))
 
     def inverse_tuple_rotation_translation(my_tuple, t, x, y):
         x_ = my_tuple[0]
@@ -168,21 +178,22 @@ def legs(leg1, leg2, leg3, leg4):
         [x_, y_] = rotation(t, x_, y_)
 
         return inverse(x_, y_, z_)
-    
+
     # Patte 1 +x,+y
-    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg1, - 3 * np.pi/4, 1, -1)
+    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg1, - 3 * np.pi / 4, 1, -1)
     targets[0:2] = [temp_x, temp_y, temp_z]
     # Patte 2 +x,+y
-    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg2, 3 * np.pi/4, 1, 1)
+    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg2, 3 * np.pi / 4, 1, 1)
     targets[3:5] = [temp_x, temp_y, temp_z]
     # Patte 3 +x,+y
-    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg3, np.pi/4, -1, 1)
+    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg3, np.pi / 4, -1, 1)
     targets[6:8] = [temp_x, temp_y, temp_z]
     # Patte 4 +x,+y
-    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg4, -np.pi/4, -1, -1)
+    temp_x, temp_y, temp_z = inverse_tuple_rotation_translation(leg4, -np.pi / 4, -1, -1)
     targets[9:] = [temp_x, temp_y, temp_z]
 
     return targets
+
 
 def walk(t, speed_x, speed_y, speed_rotation):
     d = 0.120
@@ -197,7 +208,7 @@ def walk(t, speed_x, speed_y, speed_rotation):
             speed_x, speed_y, et speed_rotation, vitesses cibles contrôlées par les sliders
     - Sortie: un tableau contenant les 12 positions angulaires cibles (radian) pour les moteurs
     """
-    targets = [0]*12
+    targets = [0] * 12
     # Position initiale
     initiale = [(-d, d, z0), (-d, -d, z0), (d, -d, z0), (d, d, z0)]
 
@@ -205,46 +216,59 @@ def walk(t, speed_x, speed_y, speed_rotation):
         return legs(initiale[0], initiale[1], initiale[2], initiale[3])
 
     # Déterminer la position des pattes en fonction de t
-    avancer_x = speed_x
+    avancer_x  = speed_x
     avancer_y = speed_y
     print("avancer_x = {0}\n avancer_y = {1}".format(avancer_x, avancer_y))
     avancer_z = 0
     if speed_x != 0 or speed_y != 0:
         avancer_z = 0.1
-    step = int((t*1000) % 4)
-    #print("t : {0}".format(t))
+    step = int((t * 100) % 4)
+    # print("t : {0}".format(t))
 
     if step == 0:
         print("step {0}".format(step))
-        patte1 = initiale[0] + (-avancer_x, -avancer_y, 0)
-        patte2 = initiale[1] + (+avancer_x / 2, +avancer_y / 2, -avancer_z)
-        patte3 = initiale[2] + (-avancer_x, -avancer_y, 0)
-        patte4 = initiale[3] + (+avancer_x / 2, +avancer_y / 2, -avancer_z)
+        patte1 = (initiale[0][0] - avancer_x, initiale[0][1] - avancer_y, initiale[0][2] + 0)
+        patte2 = (initiale[1][0] + avancer_x / 2, initiale[1][1] + avancer_y / 2, initiale[1][2] - avancer_z)
+        patte3 = (initiale[2][0] - avancer_x, initiale[2][1] - avancer_y, initiale[2][2] + 0)
+        patte4 = (initiale[3][0] + avancer_x / 2, initiale[3][1] + avancer_y / 2, initiale[3][2] - avancer_z)
 
     elif step == 1:
         print("step {0}".format(step))
-        patte1= initiale[0] + (+avancer_x / 2, +avancer_y / 2, +avancer_z)
+        patte1 = (initiale[0][0] + avancer_x / 2, initiale[0][1] + avancer_y / 2, initiale[0][2] + avancer_z)
         patte2 = initiale[1]
-        patte3 = initiale[2] + (+avancer_x / 2, +avancer_y / 2, +avancer_z)
+        patte3 = (initiale[2][0] + avancer_x / 2, initiale[2][1] + avancer_y / 2, initiale[2][2] + avancer_z)
         patte4 = initiale[3]
 
-    elif step == 2:
-        #print("step {0}".format(step))
-        patte1 = initiale[0] + (+avancer_x / 2, +avancer_y / 2, -avancer_z)
-        patte2 = initiale[1] + (-avancer_x, -avancer_y, 0)
-        patte3 = initiale[2] + (+avancer_x / 2, +avancer_y / 2, -avancer_z)
-        patte4 = initiale[3] + (-avancer_x, -avancer_y, 0)
 
-    else: # step == 3
-        print("step {0}".format(step))
-        patte1 = initiale[0] + (0, 0, 0)
-        patte2 = initiale[1] + (+avancer_x / 2, +avancer_y / 2, +avancer_z)
-        patte3 = initiale[2] + (0, 0, 0)
-        patte4 = initiale[3] + (+avancer_x / 2, +avancer_y / 2, +avancer_z)
+    elif step == 2:
+
+        # print("step {0}".format(step))
+
+        patte1 = (initiale[0][0] + avancer_x / 2, initiale[0][1] + avancer_y / 2, initiale[0][2] - avancer_z)
+
+        patte2 = (initiale[1][0] - avancer_x, initiale[1][1] - avancer_y, initiale[1][2])
+
+        patte3 = (initiale[2][0] + avancer_x / 2, initiale[2][0] + avancer_y / 2, initiale[2][0] - avancer_z)
+
+        patte4 = (initiale[3][0] - avancer_x, initiale[3][1] - avancer_y, initiale[3][2])
+
+
+    else:  # step == 3
+
+        # print("step {0}".format(step))
+
+        patte1 = initiale[0]
+
+        patte2 = (initiale[1][0] + avancer_x / 2, initiale[1][1] + avancer_y / 2, initiale[1][2] + avancer_z)
+
+        patte3 = initiale[2]
+
+        patte4 = (initiale[3][0] + avancer_x / 2, initiale[3][1] + avancer_y / 2, initiale[3][2] + avancer_z)
 
     targets = legs(patte1, patte2, patte3, patte4)
-    #print('\np1:', patte1, '\np2:', patte2, '\np3:', patte3, '\np4:', patte4)
+    # print('\np1:', patte1, '\np2:', patte2, '\np3:', patte3, '\np4:', patte4)
     return targets
+
 
 if __name__ == "__main__":
     print("N'exécutez pas ce fichier, mais simulator.py")
