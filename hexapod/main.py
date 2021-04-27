879dd442513f363dbdf16e93f255721ece47fb47
Antoine Pringalle <pringalle.a@gmail.com>
2021-04-27 11:27:23 +0200

    PAD : Marche un peu mieux mais travailler sur le passage de Simu à Réel

--- hexapod/main.py
+++ hexapod/main.py
@@ -14,12 +14,22 @@ import os
 import numpy as np
 # import display
 
+init = {11:  0, 12:-20, 13:-20,
+        21:-30, 22:-20, 23:-20,
+        31: 30, 32:-20, 33:-20,
+        41:  0, 42:-20, 43:-20,
+        51:-30, 52:-20, 53:-20,
+        61: 30, 62:-20, 63:-20}
+
+def read_init(init, value):
+    print("Value of leg {} is {}.".format(value, init[value]))
+    return init[value]
+
 def main():
     ports = pypot.dynamixel.get_available_ports()
     dxl_io = pypot.dynamixel.DxlIO(ports[0], baudrate=1000000)
     robot = SimpleRobot(dxl_io)
 
-    print(robot.legs.items())
     robot.init()
     time.sleep(0.1)
     robot.enable_torque()
@@ -32,13 +42,24 @@ def main():
         sys.exit()
         # Brutal exit
         # os._exit(1)
+    def moveFromAlphas(alphas, hasMoved, verboseMain=False):
+        for key, value in robot.legs.items():
+            value[0].goal_position = np.degrees(alphas[key-1][0])
+            value[1].goal_position = np.degrees(alphas[key-1][1])
+            value[2].goal_position = np.degrees(alphas[key-1][2])
+            if verboseMain and hasMoved:
+                print("Leg {} recieved angles T1={}, T2={}, T3={}".format(key, value[0].goal_position, value[1].goal_position, value[2].goal_position))
+            
+        if hasMoved:
+           # print("It moved!")
+            robot.smooth_tick_read_and_write(0.5, verbose=False)
 
     # Tell Python to run the shutdown() function when SIGINT is recieved
     signal(SIGINT, shutdown)
     try:
 
         params = Parameters(
-            freq=50,
+            freq=100,
             speed=1,
             z=-60,
             travelDistancePerStep=80,
@@ -49,23 +70,21 @@ def main():
         )
 
         print("Setting initial position")
-        
-        while True:
-            alphas = mouse.mouseRobot(params)
-            for key, value in robot.legs.items():
-                if key == 1 or key == 7 or key == 13 :
-                    value[0].goal_position = np.degrees(alphas[0])
-                    value[1].goal_position = -20
-                    value[2].goal_position = 0
-                else :
-                    value[0].goal_position = -np.degrees(alphas[0])
-                    value[1].goal_position = -20
-                    value[2].goal_position = 0
-            robot.smooth_tick_read_and_write(0.5, verbose=False)
+        for k, v in robot.legs.items():
+            v[0].goal_position = read_init(init, int(10 * k + 1))
+            v[1].goal_position = read_init(init, int(10 * k + 2))
+            v[2].goal_position = read_init(init, int(10 * k + 3))
+        robot.smooth_tick_read_and_write(3, verbose=False)
+        print("Init position reached")
 
+        verboseMain = True
+        while True:
+            alphas, hasMoved = mouse.mouseRobot(params=params, coef=20, verbose=True, z = 10) #input("z?"))
+
+            moveFromAlphas(alphas, hasMoved, verboseMain=False)
+            
         # TODO create this function instead:
         # setPositionToRobot(0, 0, 0, robot, params)
-        print("Init position reached")
         time.sleep(2)
         print("Closing")
     except Exception as e:
--- hexapod/mouse.py
+++ hexapod/mouse.py
@@ -4,6 +4,22 @@ import kinematics
 
 c = Controller()
 
+lastPos = None
+lastZ = None
+
+def didItMoved(pos, float_z):
+	global lastPos, lastZ
+	if lastPos == None or lastZ == None:
+		lastPos = pos
+		lastZ = float_z
+		print("First position")
+		return True
+	if lastPos[0] == pos[0] and lastPos[1] == pos[1] and float_z == lastZ:
+		return False
+	lastPos = pos
+	lastZ = float_z
+	return True
+
 def getMousePosition():
     return c.position
 
@@ -17,22 +33,27 @@ def mappingPad(value, v_min, v_max, new_min, new_max):
     value = value + new_min
     return value
 
-def mouseRobot(params):
+def mouseRobot(params, coef=10, verbose=False, z=-5):
     mp = getMousePosition()
-    mouse_x, mouse_y = mappingPad(mp[0], 0, 768, -20, 20), mappingPad(mp[1], 0, 1366, -20, 20)
-    print(mouse_x, mouse_y)
-    
+    float_z = float(z)
+    hasMoved = didItMoved(mp, float_z)
+    mouse_x, mouse_y = mappingPad(mp[0], 0, 768, -coef, coef), mappingPad(mp[1], 0, 1366, -coef, coef)
+    if verbose and hasMoved: print("Mouse in pos ({}, {}) gives: [{:.2f}, {:.2f}] (z={:.2f})".format(mp[0], mp[1], mouse_x, mouse_y, float_z))
     # Use your own IK function
+    alphas_list = []
     for leg_id in range(1, 7):
         alphas = kinematics.computeIKOriented(
             mouse_x,
             mouse_y,
-            -0,
+            float_z,
             leg_id,
             params,
             verbose=False,
         )
-    return alphas
+        alphas_list.append(alphas)
+    if verbose and hasMoved:
+    	print(alphas_list)
+    return alphas_list, hasMoved
 
 # def on_press(key):
 #     print('{0} pressed'.format(
