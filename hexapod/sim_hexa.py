3df695f1db053cee42873bb1c6fd6c9dfa1dc00e
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-05-02 19:36:58 +0200

    FEAT(robot): add rotate in holo

--- hexapod/sim_hexa.py
+++ hexapod/sim_hexa.py
@@ -60,16 +60,16 @@ def attack(params):
     Idle position.
     Its body moves with a little sinus and its front legs wave as if it was attacking someone
     """
-    angles=[]
+    angles = []
     t = time.time()
     for leg_id in range(1, 7):
 
-        if leg_id == 1 or leg_id == 2 :
-            angles.append([0.1 * math.fabs(math.tan(t)), 0, 0.1+ 0.2* math.fabs(math.cos(t))])
+        if leg_id == 1 or leg_id == 2:
+            angles.append([0.1 * math.fabs(math.tan(t)), 0, 0.1 + 0.2 * math.fabs(math.cos(t))])
         elif leg_id == 3 or leg_id == 6:
-             angles.append([0.1,  0.01 * math.cos(t),  0.01 * math.cos(t)])
-        else :
-             angles.append([0.01 * math.cos(t), 0.01 * math.cos(t), 0.01])
+            angles.append([0.1, 0.01 * math.cos(t), 0.01 * math.cos(t)])
+        else:
+            angles.append([0.01 * math.cos(t), 0.01 * math.cos(t), 0.01])
 
     for leg_id in range(1, 7):
         angle = angles[leg_id - 1]
@@ -86,30 +86,33 @@ def attack(params):
     sim.tick()
 
 
-def holowalk(x_speed, y_speed, th_speed, behaviour, params):
-    if x_speed == 0 and y_speed == 0 and th_speed == 0 and behaviour != "JUMP":
+def holowalk(x_speed, y_speed, behaviour, params):
+    if x_speed == 0 and y_speed == 0 and th_speed == 0 and behaviour != "JUMP" and behaviour != "ROTATE_R" and behaviour != "ROTATE_L":
         return "ATTACK", attack(params)
     elif x_speed != 0 or y_speed != 0:
         if x_speed > 0 and y_speed > 0:
-            return "WALK", kinematics.walkDistanceAngle(0.3, math.pi/4 , 0.15, 0.1, params)
+            return "WALK", kinematics.walkDistanceAngle(0.3, math.pi / 4, 0.15, 0.1, params)
         if x_speed < 0 and y_speed > 0:
-            return "WALK", kinematics.walkDistanceAngle(0.3, 3*math.pi/4 , 0.15, 0.1, params)
+            return "WALK", kinematics.walkDistanceAngle(0.3, 3 * math.pi / 4, 0.15, 0.1, params)
         if x_speed < 0 and y_speed < 0:
-            return "WALK", kinematics.walkDistanceAngle(0.3, 5*math.pi/4 , 0.15, 0.1, params)
+            return "WALK", kinematics.walkDistanceAngle(0.3, 5 * math.pi / 4, 0.15, 0.1, params)
         if x_speed > 0 and y_speed < 0:
-            return "WALK", kinematics.walkDistanceAngle(0.3, 7*math.pi/4 , 0.15, 0.1, params)
+            return "WALK", kinematics.walkDistanceAngle(0.3, 7 * math.pi / 4, 0.15, 0.1, params)
 
         if x_speed > 0 and y_speed == 0:
             return "WALK", kinematics.walkDistanceAngle(0.3, 0, 0.15, 0.1, params)
         if x_speed < 0 and y_speed == 0:
             return "WALK", kinematics.walkDistanceAngle(0.3, math.pi, 0.15, 0.1, params)
         if x_speed == 0 and y_speed > 0:
-            return "WALK", kinematics.walkDistanceAngle(0.3, math.pi/2, 0.15, 0.1, params)
+            return "WALK", kinematics.walkDistanceAngle(0.3, math.pi / 2, 0.15, 0.1, params)
         if x_speed == 0 and y_speed < 0:
-            return "WALK", kinematics.walkDistanceAngle(0.3, 3*math.pi/2, 0.15, 0.1, params)
+            return "WALK", kinematics.walkDistanceAngle(0.3, 3 * math.pi / 2, 0.15, 0.1, params)
     elif behaviour == "JUMP":
         return "JUMP", kinematics.jump(params=params)
-
+    elif behaviour == "ROTATE_R":
+        return "ROTATE_R", kinematics.rotate(1 * math.pi / 180, 0.15, 0.2, params)
+    elif behaviour == "ROTATE_L":
+        return "ROTATE_L", kinematics.rotate(379 * math.pi / 180, 0.15, 0.2, params)
 
 # Updates the values of the dictionnary targets to set 3 angles to a given leg
 def set_leg_angles(alphas, leg_id, targets, params):
@@ -143,8 +146,8 @@ def print_points(points):
     for pt in points:
         # Drawing each step of the DK calculation
         i += 1
-        T.append(kinematics.rotaton_2D(pt[0], pt[1] , pt[2], leg_angle))
-        T[-1][0] += leg_center_pos[0] # Ajout l'offset de l'épaule
+        T.append(kinematics.rotaton_2D(pt[0], pt[1], pt[2], leg_angle))
+        T[-1][0] += leg_center_pos[0]  # Ajout l'offset de l'épaule
         T[-1][1] += leg_center_pos[1]
         T[-1][2] += leg_center_pos[2]
         print("Drawing cross {} at {}".format(i, T[-1]))
@@ -152,17 +155,20 @@ def print_points(points):
             crosses[i], T[-1], to_pybullet_quaternion(0, 0, leg_angle)
         )
 
+
 def inverseUpdate(controls):
     x = p.readUserDebugParameter(controls[0])
     y = p.readUserDebugParameter(controls[1])
     z = p.readUserDebugParameter(controls[2])
     p.resetBasePositionAndOrientation(
-        controls[3], [x, y, z+0.1], p.getQuaternionFromEuler([0, 0, 0]))
+        controls[3], [x, y, z + 0.1], p.getQuaternionFromEuler([0, 0, 0]))
     return x, y, z
+
 
 last_angles = []
 
-def from_list_to_simu(list_of_angles, dt = 1/1000):
+
+def from_list_to_simu(list_of_angles, dt=1 / 1000):
     global last_angles
     for step in list_of_angles:
         smooth_steps = kinematics.make_smooth(step, last_angles, smooth_num=10)
@@ -178,6 +184,7 @@ def from_list_to_simu(list_of_angles, dt = 1/1000):
                 state = sim.setJoints(targets)
                 sim.tick()
                 time.sleep(dt)
+
 
 if args.mode == "frozen-direct":
     crosses = []
@@ -204,7 +211,8 @@ elif args.mode == "inverse":
     for i in range(5):
         crosses.append(p.loadURDF("target2/robot.urdf"))
     # Use your own DK function
-    alphas = kinematics.computeDK(0, 0, 0, use_rads=True) # [0, constants.THETA2_MOTOR_SIGN * constants.theta2Correction, constants.THETA3_MOTOR_SIGN * constants.theta3Correction]
+    alphas = kinematics.computeDK(0, 0, 0,
+                                  use_rads=True)  # [0, constants.THETA2_MOTOR_SIGN * constants.theta2Correction, constants.THETA3_MOTOR_SIGN * constants.theta3Correction]
     controls["target_x"] = p.addUserDebugParameter("target_x", -0.4, 0.4, alphas[0])
     controls["target_y"] = p.addUserDebugParameter("target_y", -0.4, 0.4, alphas[1])
     controls["target_z"] = p.addUserDebugParameter("target_z", -0.4, 0.4, alphas[2])
@@ -224,7 +232,7 @@ elif args.mode == "holo":
 elif args.mode == "walk-jump":
     last_angles = 18 * [0]
 
-dt = 1/10000
+dt = 1 / 10000
 
 while True and "walk" not in args.mode and "holo" not in args.mode and "rotate" not in args.mode:
     tick = 1
@@ -288,7 +296,7 @@ while True and "walk" not in args.mode and "holo" not in args.mode and "rotate" 
         T[2] += leg_center_pos[2]
         # print("Drawing cross {} at {}".format(i, T))
         p.resetBasePositionAndOrientation(
-            crosses [-1], T, to_pybullet_quaternion(0, 0, leg_angle)
+            crosses[-1], T, to_pybullet_quaternion(0, 0, leg_angle)
         )
 
     elif args.mode == "robot-ik":
@@ -327,7 +335,7 @@ while True and "walk" not in args.mode and "holo" not in args.mode and "rotate" 
             z_body = max(z_body - value, params.z - max_value)
 
         # print("{}, {}, {}".format(x_body, y_body, z_body))
-        
+
         for leg_id in range(1, 7):
             alphas = kinematics.computeIKOriented(
                 x_body,
@@ -348,7 +356,7 @@ while True and "walk" not in args.mode and "holo" not in args.mode and "rotate" 
     elif args.mode == "attack":
         angles = attack(params)
         for leg_id in range(1, 7):
-            angle = angles[leg_id-1]
+            angle = angles[leg_id - 1]
             alphas = kinematics.computeIKOriented(
                 angle[0],
                 angle[1],
@@ -380,7 +388,7 @@ elif args.mode == "rotate":
     tick = 1
     targets = {}
     t = time.time()
-    sample = kinematics.rotate(math.pi/2, 0.1, 0.1, params)
+    sample = kinematics.rotate(math.pi / 2, 0.1, 0.1, params)
     print("time to compute :", time.time() - t)
     print("sample : ", sample)
     t = time.time()
@@ -390,13 +398,13 @@ elif args.mode == "rotate":
 elif args.mode == "walk-configurable":
     tick = 1
     targets = {}
-    while(1):
-        angle = (math.pi/180)*p.readUserDebugParameter(controls["angle"])
+    while (1):
+        angle = (math.pi / 180) * p.readUserDebugParameter(controls["angle"])
         step_dist = p.readUserDebugParameter(controls["step_dist"])
         dt_factor = p.readUserDebugParameter(controls["freq"])
         if step_dist != 0:
-            sample = kinematics.walkDistanceAngle(step_dist*2, angle, step_dist, 0.1, params)
-            from_list_to_simu(sample, 1/dt_factor)
+            sample = kinematics.walkDistanceAngle(step_dist * 2, angle, step_dist, 0.1, params)
+            from_list_to_simu(sample, 1 / dt_factor)
         else:
             attack(params)
 
@@ -406,32 +414,35 @@ elif args.mode == "holo":
     t = time.time()
     while True:
         keys = p.getKeyboardEvents()
-        x_speed, y_speed, th_speed = 0, 0, 0
+        x, y, th_speed = 0, 0, 0
         if behaviour == "ATTACK":
             if 122 in keys:
-                x_speed = 10
+                x = 10
             elif 115 in keys:
-                x_speed = -10
+                x = -10
 
             if 113 in keys:
-                y_speed = -10
+                y = -10
             elif 100 in keys:
-                y_speed = 10
+                y = 10
 
             if 101 in keys:
-                th_speed = 1
+                behaviour = "ROTATE_R"
             elif 97 in keys:
-                th_speed = -1
+                behaviour = "ROTATE_L"
 
             if 32 in keys and keys[32] == 4:
                 behaviour = "JUMP"
 
-        behaviour, angles = holowalk(x_speed, y_speed, th_speed, behaviour, params)
+        behaviour, angles = holowalk(x, y, behaviour, params)
         if behaviour == "ATTACK":
             attack(params)
-        elif behaviour == "WALK" :
+        elif behaviour == "WALK":
             from_list_to_simu(angles)
         elif behaviour == "JUMP":
+            from_list_to_simu(angles)
+            behaviour = "ATTACK"
+        elif behaviour == "ROTATE_R" or behaviour == "ROTATE_L":
             from_list_to_simu(angles)
             behaviour = "ATTACK"
 
@@ -439,7 +450,7 @@ elif args.mode == "holo":
 elif args.mode == "walk-jump":
     tick = 1
     targets = {}
-    while(1):
+    while (1):
         keys = p.getKeyboardEvents()
         # On trigger le jump que quand on release la barre espace
         # Pour trigger quand on appuie dessus, mettre keys[32] == 3
