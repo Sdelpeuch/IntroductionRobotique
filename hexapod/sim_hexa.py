bf42ee45cae34dde2abf6d07da27e22995706b92
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-05-03 12:07:51 +0200

    FIX(holo): fix rotate param holo mode

--- hexapod/sim_hexa.py
+++ hexapod/sim_hexa.py
@@ -110,9 +110,9 @@ def holowalk(x_speed, y_speed, behaviour, params):
     elif behaviour == "JUMP":
         return "JUMP", kinematics.jump(params=params)
     elif behaviour == "ROTATE_R":
-        return "ROTATE_R", kinematics.rotate(1 * math.pi / 180, 0.15, 0.2, params)
+        return "ROTATE_R", kinematics.rotate(0.15*2, 0.15, 0.2, params)
     elif behaviour == "ROTATE_L":
-        return "ROTATE_L", kinematics.rotate(379 * math.pi / 180, 0.15, 0.2, params)
+        return "ROTATE_L", kinematics.rotate(-0.15*2, 0.15, 0.2, params)
 
 # Updates the values of the dictionnary targets to set 3 angles to a given leg
 def set_leg_angles(alphas, leg_id, targets, params):
